f8f86f94f4a43cb12ea1b26c147b3c8582b52b6e
nbeverton <nbeverton@gmail.com>
2023-05-08 19:01:45 -0300

    Construção do Display
    Construção dos elementos da Classe Display. Inserção dos parâmetros e customização.

--- src/br/com/nbeverton/calc/visao/Display.java
+++ src/br/com/nbeverton/calc/visao/Display.java
@@ -5,8 +5,18 @@ import java.awt.*;
 
 public class Display extends JPanel {
 
+    private final JLabel label;
+
     public Display() {
-        setBackground(Color.GREEN);
+         label = new JLabel("1234,56");
+
+         setBackground(new Color(46, 49, 50));
+         label.setForeground(Color.WHITE);
+         label.setFont(new Font("courier", Font.PLAIN, 30));
+
+         setLayout(new FlowLayout(FlowLayout.RIGHT, 10, 25));
+
+         add(label);
     }
 
 }
