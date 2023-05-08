b7ca1994cb209171bb6551eacd961a86f29d7080
nbeverton <nbeverton@gmail.com>
2023-05-08 19:28:34 -0300

    Componente Botão
    Construção da classe Botão.

--- src/br/com/nbeverton/calc/visao/Botao.java
+++ src/br/com/nbeverton/calc/visao/Botao.java
@@ -1,8 +1,18 @@
 package br.com.nbeverton.calc.visao;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Botao extends JButton {
 
+    public Botao(String texto, Color cor) {
+        setText(texto);
+        setOpaque(true);
+        setBackground(cor);
+        setForeground(Color.WHITE);
+        setBorder(BorderFactory.createLineBorder(Color.BLACK));
 
+        setFont(new Font("courier", Font.PLAIN, 25));
+
+    }
 }
--- src/br/com/nbeverton/calc/visao/Teclado.java
+++ src/br/com/nbeverton/calc/visao/Teclado.java
@@ -5,7 +5,12 @@ import java.awt.*;
 
 public class Teclado extends JPanel {
 
+    private final Color COR_CINZA_ESCURO = new Color(68, 68, 68);
+    private final Color COR_CINZA_CLARO = new Color(99, 99, 99);
+    private final Color COR_LARANJA = new Color(243, 163, 60);
+
     public Teclado() {
-        setBackground(Color.RED);
+
+        
     }
 }
